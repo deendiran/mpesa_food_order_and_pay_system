82fdfcb9ccc915315d72146ebe1cf81872e63104
deendiran <ndirangudenise61@gmail.com>
2026-07-07 21:09:43 +0300

    new changes

--- app.py
+++ app.py
@@ -11,7 +11,14 @@ app = Flask(__name__)
 CORS(app)
 
 # Configure SQLite database
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///nourish_net.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///nourish_net.db"
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///nourish_net.db"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "2898db2a80a4f110e39490e2de8425c8c2523045587c08f1"
 
@@ -29,16 +36,22 @@ app.config["MPESA_ACCESS_TOKEN_URL"] = (
 )
 app.config["MPESA_STK_PUSH_URL"] = "/mpesa/stkpush/v1/processrequest"
 app.config["MPESA_STK_QUERY_URL"] = "/mpesa/stkpushquery/v1/query"
-app.config["MPESA_CONSUMER_KEY"] = "sB2Gdv6pqxdDD62uxZhsWS3f0hM01EPGdpw6cDwgdypAOZOw"
-app.config["MPESA_CONSUMER_SECRET"] = (
-    "ETPTjx8zZZKl7Mce4QsGJkMLWLJX8TYx8HxrY5qadqQFVAXy5GAEovL5tsbCCGtN"
-)
-app.config["MPESA_PASSKEY"] = (
-    "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-)
+# app.config["MPESA_CONSUMER_KEY"] = "sB2Gdv6pqxdDD62uxZhsWS3f0hM01EPGdpw6cDwgdypAOZOw"
+# app.config["MPESA_CONSUMER_SECRET"] = (
+#     "ETPTjx8zZZKl7Mce4QsGJkMLWLJX8TYx8HxrY5qadqQFVAXy5GAEovL5tsbCCGtN"
+# )
+# app.config["MPESA_PASSKEY"] = (
+#     "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+# )
 app.config["MPESA_TILL_NUMBER"] = "174379"
 app.config["MPESA_BUSINESS_SHORT_CODE"] = "174379"
-app.config["MPESA_CALLBACK_URL"] = "https://d864-41-90-172-126.ngrok-free.app/callback"
+# app.config["MPESA_CALLBACK_URL"] = "https://d864-41-90-172-126.ngrok-free.app/callback"
+
+"""Alternative"""
+app.config["MPESA_CONSUMER_KEY"] = os.getenv("MPESA_CONSUMER_KEY")
+app.config["MPESA_CONSUMER_SECRET"] = os.getenv("MPESA_CONSUMER_SECRET")
+app.config["MPESA_PASSKEY"] = os.getenv("MPESA_PASSKEY")
+app.config["MPESA_CALLBACK_URL"] = os.getenv("MPESA_CALLBACK_URL")
 
 # Initialize db with app
 db.init_app(app)
